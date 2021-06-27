567883dfb5fc80c692a6fea5fbab3df6685bc48b
tawfiq <tawfiq_kh@yahoo.com>
2021-06-27 12:01:16 +0300

    finish

--- src/components/Fentity.js
+++ src/components/Fentity.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import '../styles/fentity.css'
-
+import React, { Component } from "react";
+import "../styles/fentity.css";
 
 class Fentity extends Component {
-    render() {
-        return (
-            <div id="creature-container">
-                <h1>{fentity.name}</h1>
-                <img src={fentity.imgUrl} alt=""/>
-                <div className="title">Power:</div>
-                <div className="power text"> {fentity.power}</div>
-                <div className="other text">{fentity.other}</div>
-            </div>
-        )
-    }
+  render() {
+    const fentityCategory = this.props.match.params.fentities;
+    const fentity = this.props.state[fentityCategory].filter(
+      (f) => f.name.toLowerCase() === this.props.match.params.name
+    )[0];
+    console.log(fentity);
+    return (
+      <div id="creature-container">
+        <h1>{fentity.name}</h1>
+        <img src={fentity.imgUrl} alt="" />
+        <div className="title">Power:</div>
+        <div className="power text"> {fentity.power}</div>
+        <div className="other text">{fentity.other}</div>
+      </div>
+    );
+  }
 }
 
 export default Fentity;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import '../styles/home.css'
+import "../styles/home.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class Home extends Component {
+  render() {
+    return (
+      <div>
+        <div id="u-input">
+          <input type="text" placeholder="Ask and you shall receive" />
+          <div id="button">Seek</div>
+        </div>
 
-    render() {
-        return (
-            <div>
-                <div id="u-input">
-                    <input type="text" placeholder="Ask and you shall receive" />
-                    <div id="button">Seek</div>
-                </div>
+        <h1 id="home-title">Your Adventure</h1>
 
-                <h1 id="home-title">Your Adventure</h1>
-
-                <div id="home-container">
-                    <div id="world"><span className="main-directory-text">World</span></div>
-                    <div id="wizards"><span className="main-directory-text">Wizards</span></div>
-                    <div id="bestiary"><span className="main-directory-text">Bestiary</span></div>
-                    <div id="potions"><span className="main-directory-text">Potions</span></div>
-                    <div id="deities"><span className="main-directory-text">Deities</span></div>
-                </div>
-            </div>
-        );
-    }
+        <div id="home-container">
+          <div id="world">
+            <span className="main-directory-text">
+              <Link to="/directory/world">World</Link>
+            </span>
+          </div>
+          <div id="wizards">
+            <span className="main-directory-text">
+              <Link to="/directory/wizards">Wizards</Link>
+            </span>
+          </div>
+          <div id="bestiary">
+            <span className="main-directory-text">
+              <Link to="/directory/bestiary">Bestiary</Link>
+            </span>
+          </div>
+          <div id="potions">
+            <span className="main-directory-text">
+              <Link to="/directory/potions">Potions</Link>
+            </span>
+          </div>
+          <div id="deities">
+            <span className="main-directory-text">
+              <Link to="/directory/deities">Deities</Link>
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
